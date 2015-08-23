080454f2f0e3542ebda64d1980c7e1f1f7b1e7be
Marcos <marcos.bcc2011@gmail.com>
2015-08-23 19:05:30 -0300

    Coded constructor for the class Client

--- Client.java
+++ Client.java
@@ -15,6 +15,25 @@ public class Client extends JFrame{
 	private String serverIP;
 	private Socket connection;
 
-	
+	//constructor
+	public Client(String host){
+		super("Client mofo!");
+		serverIP = host;
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					sendData(event.getActionCommand());
+					userText.setText("");
+				}
+			}
+		);
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow), BorderLayout.CENTER);
+		setSize(300,150);
+		setVisible(true);
+	}
 
 }
