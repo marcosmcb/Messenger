f1c1f79ce0564f9fecc2b444ecde730c01ca7a22
marcosmcb <marcos.bcc2011@gmail.com>
2015-08-23 17:51:07 -0300

    Created class Server

---
+++ Server.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.net.*;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class Server extends JFrame{
+	
+	private JTextField userText;
+	private JTextArea chatWindow;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	private ServerSocket server;
+	private Socket connection;
+
+	//constructor
+	public Server(){
+		super("Buckys Instant Messenger");
+		userText = new JTextField();
+		userText.setEditable(false);
+		userText.addActionListener(
+			new ActionListener(){
+				public void actionPerformed(ActionEvent event){
+					sendMessage(event.getActionCommand());
+					userText.setText("");
+				}
+			}
+		);
+		add(userText, BorderLayout.NORTH);
+		chatWindow = new JTextArea();
+		add(new JScrollPane(chatWindow));
+		setSize(300,150);
+		setVisible(true);
+	}
+
+	//set up and run the server
+	public void startRunning(){
+		try{
+			server = new ServerSocket(6789, 100);
+		}catch(IOException ioException){	
+			ioException.printStackTrace();
+		}
+	}
+
+
+}
